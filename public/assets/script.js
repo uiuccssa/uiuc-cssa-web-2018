0fb7a5d97d3e984445684cad1d66816f5f8e861b
jettairlines <lwanghe2@gmail.com>
2017-11-05 19:39:08 -0600

    基本框架撸好了

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,45 +1,42 @@
-var content = {
-    "pages" : "home",
-    "sections" : {
-        "1" : "<p>Hello</p>",
-        "2" : "<p>world.</p>"
-    }
-}
-
-
-//=====================================================
-
+/*
+* Helper functions to determine
+* what files to read from a given link
+*/
+// returns a directory path
 function hrefToFile(href){
     return ( href? href.substr(1)+'.html' : "home.html" );
 }
-function setBody(href){
-    $("nav li").removeClass('active');
+// Refreshes the body content
+function setBody(name,href){
+    $("nav li").removeClass("active");
     
     $("#body").empty();
     $("#body").attr("w3-include-html", hrefToFile(href));
     w3.includeHTML();
-    
+    $("title").text("UIUC CSSA | "+name );
     //alert('body set to'+href);
 }
 
+/*
+* jQuery function to manipulate the page when it's loaded
+*/
 $(document).ready(function() {
 
-    // set location to read file
+    // Pulls a page from a link when it's first opened
     var href_curr = window.location.hash;
     setBody(href_curr);   
     
     //Listen for clicks on navbar
-    $("nav li").click(function(event) {
-        //var href_out = $(this).attr('href');
-        setBody($(this).attr('href'));
-        $(this).addClass('active');
+    $("nav li>a").not("nav li:last-child").click(function(event) {
+        var href_out = $(this).attr("href");
+        var name = $(this).parentsUntil("nav ul.dropdown-menu").first().text();
+        
+        // if a is not a link, dont do anything
+        if (!href_out) { return; }
+        
+        setBody(name,href_out);
+        $(this).parent().addClass("active");
+
     });
 
 });
-
-
-/*
-for(var e in content.sections) {
-    $("#body").append(content.sections[e]);
-}
-*/
